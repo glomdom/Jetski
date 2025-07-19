483780926a9a274ffbf516235b6f81a41674353b
glomdom <22115@uktc-bg.com>
2025-07-19 19:21:35 +0300

    fix: use data class instead of class

--- src/main/kotlin/pe/Image.kt
+++ src/main/kotlin/pe/Image.kt
@@ -1,14 +1,14 @@
 package io.github.glomdom.jetski.pe
 
-class Image {
-    var architecture: TargetArchitecture? = null
-    var timestamp: UInt? = null
-    var kind: ModuleKind? = null
-    var characteristics: UShort? = null
-    var linkerVersion: UShort? = null
-    var subSystemMajor: UShort? = null
-    var subSystemMinor: UShort? = null
-    var win32Resources: DataDirectory? = null
-    var debug: DataDirectory? = null
-    var sections: MutableList<Section>? = null
-}
+data class Image(
+    var architecture: TargetArchitecture? = null,
+    var timestamp: UInt? = null,
+    var kind: ModuleKind? = null,
+    var characteristics: UShort? = null,
+    var linkerVersion: UShort? = null,
+    var subSystemMajor: UShort? = null,
+    var subSystemMinor: UShort? = null,
+    var win32Resources: DataDirectory? = null,
+    var debug: DataDirectory? = null,
+    var sections: List<Section> = emptyList()
+)
